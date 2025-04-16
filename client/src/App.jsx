2e98ff3eaef0a090750a7bb23933c73a339e7d65
Shivraj joshi <shibujoshi07@gmail.com>
2025-04-17 02:12:27 +0530

    client folder structure setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Student Info Tracker</h1>
+        <h1 className="bg-[#EDDDDD] p-3 text-center">Student Info Tracker</h1>
       </div>
     </>
   );
