b8b15e310b9854bb24952410d2e857a07dc74dd5
Shivraj joshi <shibujoshi07@gmail.com>
2026-06-06 23:44:01 +0530

    folder structure created

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,15 @@ import express from 'express'
 import helmet from 'helmet'
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(helmet())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.json({ message: 'Hello World' })
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
